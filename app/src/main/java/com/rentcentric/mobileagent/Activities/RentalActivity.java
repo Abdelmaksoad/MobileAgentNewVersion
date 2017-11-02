efd667f6fc66d751e875e428f5b549c1a5daa73d
DevMohamedMohsen <mohammed.mohsein@gmail.com>
2017-11-02 17:01:02 +0200

    login

---
+++ app/src/main/java/com/rentcentric/mobileagent/Activities/RentalActivity.java
@@ -0,0 +1,15 @@
+package com.rentcentric.mobileagent.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rentcentric.mobileagent.R;
+
+public class RentalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rental);
+    }
+}
