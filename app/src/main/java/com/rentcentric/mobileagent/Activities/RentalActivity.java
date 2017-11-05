0a969e84c894afcbd7957aa65ebe5f027b1dd39f
DevMohamedMohsen <mohammed.mohsein@gmail.com>
2017-11-05 17:20:14 +0200

    rental done

--- app/src/main/java/com/rentcentric/mobileagent/Activities/RentalActivity.java
+++ app/src/main/java/com/rentcentric/mobileagent/Activities/RentalActivity.java
@@ -14,7 +14,7 @@ import com.rentcentric.mobileagent.R;
 public class RentalActivity extends AppCompatActivity {
 
     TextView Checkin, Checkout;
-    boolean in,out;
+    boolean in = true, out;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,33 +23,48 @@ public class RentalActivity extends AppCompatActivity {
         Checkin = (TextView) findViewById(R.id.checkin);
         Checkout = (TextView) findViewById(R.id.checkout);
         getFragmentManager().beginTransaction().add(R.id.RentalContainer, new RentalCheckInFragment()).commit();
+        CheckinChecked();
 
         Checkin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getFragmentManager().beginTransaction().replace(R.id.RentalContainer, new RentalCheckInFragment()).commit();
-                Checkin.setBackground(getResources().getDrawable(R.drawable.textview));
-                Checkin.setTextColor(Color.parseColor("#3399FE"));
-                in=true;
-                out=false;
+                in = true;
+                CheckinChecked();
+                out = false;
+                CheckoutChecked();
             }
         });
 
         Checkout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getFragmentManager().beginTransaction().replace(R.id.RentalContainer, new RentalCheckOutFragment()).commit();
-                Checkout.setBackground(getResources().getDrawable(R.drawable.textview));
-                Checkout.setTextColor(Color.parseColor("#3399FE"));
-                out=true;
-                in=false;
+                out = true;
+                CheckoutChecked();
+                in = false;
+                CheckinChecked();
             }
         });
     }
 
-    private void CheckinChecked(){
-        if (in==true){
+    private void CheckinChecked() {
+        if (in == true) {
+            getFragmentManager().beginTransaction().replace(R.id.RentalContainer, new RentalCheckInFragment()).commit();
+            Checkin.setBackground(getResources().getDrawable(R.drawable.textview));
+            Checkin.setTextColor(getResources().getColor(R.color.colorAccent));
+        } else {
+            Checkin.setBackground(getResources().getDrawable(R.drawable.tab));
+            Checkin.setTextColor(getResources().getColor(R.color.colorWhite));
+        }
+    }
 
+    private void CheckoutChecked() {
+        if (out == true) {
+            getFragmentManager().beginTransaction().replace(R.id.RentalContainer, new RentalCheckOutFragment()).commit();
+            Checkout.setBackground(getResources().getDrawable(R.drawable.textview));
+            Checkout.setTextColor(getResources().getColor(R.color.colorAccent));
+        } else {
+            Checkout.setBackground(getResources().getDrawable(R.drawable.tab));
+            Checkout.setTextColor(getResources().getColor(R.color.colorWhite));
         }
     }
 }
